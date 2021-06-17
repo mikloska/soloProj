1dfacb199a2355b69801e94ed1d37b3daa060385
mikloska <mikloska973@gmail.com>
2021-06-17 17:16:58 -0400

    sound works and so does production server thanks to Randy

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose'; //either is valid
+import dotenv from 'dotenv'
+
+
 const app = express();
-const config = require('../webpack.config');
-const compiler = webpack(config);
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+dotenv.config(); //what is this shit
+app.use(express.static('assets'))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors);
+
 app.get('/', (req, res) => {
     res.send
+
 })
-// Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!\n');
-});
+
+const port = process.env.PORT || 5000;
+//console.log('connection url: ', process.env.CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=> console.log('connected to db'))
+  .catch(err => console.log(err))
+
+app.listen(port, ()=> console.log(`Listening on ${port}`))
+
+mongoose.set('useFindAndModify', false)
